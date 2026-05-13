b62d2c12386be2a8db4fe346feaa7b2fcb501391
kaneflores <kaneflores911@gmail.com>
2026-05-13 16:59:39 -0400

    made comments

--- volume_helper.c
+++ volume_helper.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 //
+/*
+    volume_helper.c
+    Windows per_application volume controller using Core audio (WASAPI).
+    Compile with MinGW:
+        gcc volume
 
+
+*/
 
 int main(){
     return 0;
