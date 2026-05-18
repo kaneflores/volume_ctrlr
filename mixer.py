913a1c43846d5279069d97ba922e683e2748cdff
kaneflores <kaneflores911@gmail.com>
2026-05-18 19:55:12 -0400

    init msg

--- mixer.py
+++ mixer.py
@@ -1,3 +1,7 @@
 """
+mixer.py - Per-application volume mixer for Windows
+Requires: Python 3.8+, tkinter (bundled with python)
+The compiled volume_helper.exe must sit in the same folder as the script.
 
+Run: python mixer.py
 """
